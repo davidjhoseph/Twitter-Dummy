1558ae67fcab2dea8a5c5cf25657e003f35e2caf
David Joseph <davidjoe781227@gmail.com>
2020-04-25 22:51:33 +0100

    ...

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,11 +1,4 @@
 @extends('layout')
 @section('content')
-    @if(Session::get('useremail') !== null)
-        <div>Welcome {{ Session::get('useremail') }}</div>
-    @else
-        {{-- <script>window.location="/login";</script> --}}
-        <div>You are not logged in!</div>
-    @endif
-
-    {{-- <dashboard></dashboard> --}}
+    <dashboard></dashboard>
 @endsection
